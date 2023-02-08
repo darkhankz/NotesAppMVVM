e8652320ad7bcf38832f820b41dec6e1f36b5c07
darkhankz <upstudynow@gmail.com>
2023-02-08 17:09:46 +0600

    CreateNav to End

---
+++ app/src/main/java/com/example/notesappmvvm/screens/Main.kt
@@ -0,0 +1,10 @@
+package com.example.notesappmvvm.screens
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun Main(navController: NavHostController) {
+    Text(text = "Main screen")
+}
